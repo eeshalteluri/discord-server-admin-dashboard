39657a7b6173d10a2f33a94c6e039e2aaba567a1
eeshalteluri <eeshalteluri@gmail.com>
2025-06-19 23:54:09 +0530

    Updated home redirect route to dashboard route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
-
+import { redirect } from "next/navigation";
 export default function Home() {
+  redirect('/dashboard')
   return (
     <div className="flex-1 flex flex-col">
       <div className="flex-1">
